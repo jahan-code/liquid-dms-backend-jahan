549aaf680189f6a6e223183d0801211856fb1a7c
Jahanzaib <jahanzaib9599@gmail.com>
2025-07-21 15:07:58 +0500

    Date reset

--- src/models/vehicle.js
+++ src/models/vehicle.js
@@ -208,7 +208,7 @@ const vehicleSchema = new mongoose.Schema(
       otherImageUrls: [String],
     },
     costDetails: {
-      purchaseDate: { type: Date, default: Date.now },
+      purchaseDate: Date,
       originalCost: { type: Number, default: 0 },
       buyersFee: { type: Number, default: 0 },
       transportationFee: { type: Number, default: 0 },
