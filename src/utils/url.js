c540ac1b4d6b15a142818e65a4afa2484706fff0
Jahanzaib <141170635+jahan-code@users.noreply.github.com>
2025-08-20 12:38:11 +0500

    changing image url

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8001';
+const BASE_URL = 'https://liquid.thedevapp.online';
 export function getFullImageUrl(filename) {
   return `${BASE_URL}/uploads/${filename}`;
 }
