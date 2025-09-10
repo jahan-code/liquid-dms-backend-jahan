cddddbebd832c78820e9a8bdc580a57413d33c67
jahan-code <jahanzaib9599@gmail.com>
2025-09-10 13:52:15 +0500

    token verification on customers

--- src/router/customer.js
+++ src/router/customer.js
@@ -8,13 +8,14 @@ import {
   getAllCustomersWithoutPagination,
   getCustomerById,
 } from '../controllers/customerController.js';
+import verifyToken from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/add-customer', addCustomer);
-router.get('/', getCustomerById);
-router.put('/edit-customer', editCustomerById);
-router.get('/customers', getAllCustomers);
-router.get('/customers/all', getAllCustomersWithoutPagination);
-router.delete('/delete-customer', deleteCustomerById);
+router.post('/add-customer', verifyToken, addCustomer);
+router.get('/', verifyToken, getCustomerById);
+router.put('/edit-customer', verifyToken, editCustomerById);
+router.get('/customers', verifyToken, getAllCustomers);
+router.get('/customers/all', verifyToken, getAllCustomersWithoutPagination);
+router.delete('/delete-customer', verifyToken, deleteCustomerById);
 export default router;
